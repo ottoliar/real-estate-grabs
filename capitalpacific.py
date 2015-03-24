3ac46558648e5ac976d1ae16dae486a37aaa7f2a
Robert Ottolia <ottoliarobert@gmail.com>
2015-03-23 17:37:24 -0700

    Update capitalpacific.py

--- capitalpacific.py
+++ capitalpacific.py
@@ -55,7 +55,7 @@ else:
     fromaddr = 'ottoliarobert@gmail.com'
     toaddrs = ['ottoliar@onid.oregonstate.edu']
     username = 'ottoliarobert@gmail.com'
-    password = '08Acuratl'
+    password = 'xxxxxxxxx'
 
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.starttls()
