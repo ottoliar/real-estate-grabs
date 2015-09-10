c22aba5152aec5158b2b54bac87395824ffab168
unknown <ottoliarobert@gmail.com>
2015-09-09 23:00:54 -0700

    HFO Real Estate

---
+++ hfo.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+import pymongo
+import sys
+import smtplib
+
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+
+db = client.properties
+collection = db['hfo']
+
+oldProperties = []
+newProperties = []
+
+driver = webdriver.Firefox()
+
+driver.get("http://hfore.com/transactions.aspx?show=listings")
+
+for property in driver.find_elements_by_class_name('propertyItem'):
+	info = property.find_element_by_tag_name('h3')
+	links = property.find_element_by_tag_name('a')
+	URL = links.get_attribute('href')
+	title = info.text
+	
+	newProperties.append({
+		'URL': URL,
+		'title': title
+		})
+
+driver.close()
+
+if collection.count != 0:
+	for post in collection.find():
+		oldProperties.append(post)
+
+	for oldListing in oldProperties:
+		for newListing in newProperties:
+			if oldListing['URL'] == newListing['URL']:
+				newProperties.remove(newListing)
+
+if len(newProperties) == 0:
+	sys.exit()
+else:
+	with open('passwords.txt') as inFile:
+		password = inFile.read()
+	fromaddr = 'commercialinvestorsofus@gmail.com'
+	toaddrs = ['andy.ottolia@svn.com']
+	username = fromaddr
+	subject = "New Listing @ HFO Real Estate"
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.login(username, password)
+
+	for item in newProperties:
+		msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" 
+			% (fromaddr, ", ".join(toaddrs), subject) )
+		msg += item['title'] + "\n"
+		msg += item['URL']
+		server.sendmail(fromaddr, toaddrs, msg.encode('utf-8'))
+		collection.insert(item)
+
+server.quit()
